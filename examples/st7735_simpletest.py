c57ebf09847091d0d8230d7578de9b86f3799709
dherrada <dylan.herrada@gmail.com>
2020-03-09 20:05:29 -0400

    Ran black, updated to pylint 2.x

--- examples/st7735_simpletest.py
+++ examples/st7735_simpletest.py
@@ -14,7 +14,9 @@ spi = board.SPI()
 tft_cs = board.D5
 tft_dc = board.D6
 
-display_bus = displayio.FourWire(spi, command=tft_dc, chip_select=tft_cs, reset=board.D9)
+display_bus = displayio.FourWire(
+    spi, command=tft_dc, chip_select=tft_cs, reset=board.D9
+)
 
 display = ST7735(display_bus, width=128, height=128)
 
@@ -26,9 +28,7 @@ color_bitmap = displayio.Bitmap(128, 128, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0xFF0000
 
-bg_sprite = displayio.TileGrid(color_bitmap,
-                               pixel_shader=color_palette,
-                               x=0, y=0)
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
 splash.append(bg_sprite)
 
 while True:
